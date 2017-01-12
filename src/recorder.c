8f9714c0da0ab08b0902fa01878c166ca5b09d8e
weijie.yuan <weijie.yuan@aispeech.com>
2017-01-12 10:22:19 +0800

    support recorder

--- src/recorder.c
+++ src/recorder.c
@@ -1,8 +1,8 @@
 /*************************************************************************
-	> File Name: player.c
+	> File Name: recorder.c
 	> Author: weijie.yuan
 	> Mail: yuanweijie1993@gmail.com
-	> Created Time: Wed 11 Jan 2017 02:50:13 PM CST
+	> Created Time: Thu 12 Jan 2017 09:45:02 AM CST
  ************************************************************************/
 
 #include <stdio.h>
@@ -28,7 +28,7 @@ int main(int argc,const char *argv[])
 	snd_pcm_hw_params_t *params;
 
 	/* open devices */
-	rc = snd_pcm_open(&handle,device,SND_PCM_STREAM_PLAYBACK, 0); // open pcm device for playback
+	rc = snd_pcm_open(&handle,device,SND_PCM_STREAM_CAPTURE, 0); // open pcm device for capture
 	if (rc < 0)
 	{
 		fprintf(stderr,"unable to open pcm device: %s\n",snd_strerror(rc));
@@ -43,7 +43,7 @@ int main(int argc,const char *argv[])
     snd_pcm_hw_params_set_access(handle, params, SND_PCM_ACCESS_RW_INTERLEAVED); // interleaved mode
     snd_pcm_hw_params_set_format(handle, params, SND_PCM_FORMAT_S16_LE); //format
     snd_pcm_hw_params_set_channels(handle, params, channels);
-    snd_pcm_hw_params_set_rate_near(handle, params, &sample_rate, &dir);
+    snd_pcm_hw_params_set_rate(handle, params, sample_rate, 0);
     snd_pcm_hw_params_set_period_size_near(handle, params, (snd_pcm_uframes_t *)&frames, &dir);
 
     /* write the params to the driver */
@@ -59,34 +59,23 @@ int main(int argc,const char *argv[])
     size_t size = frames * channels * (16/8); //sample*channels*bytes/(one channel)
     char *buffer = (char *) acalloc(1,size);
 
-    FILE *fp = fopen(argv[1],"rb");
+    unsigned int val;
+    long loops;
+
+    snd_pcm_hw_params_get_period_time(params,&val,&dir);
+    loops = 5000000 / val; // how many periods in 5s
+
+    FILE *fp = fopen(argv[1],"wb");
     if (fp == NULL)
     {
     	fprintf(stderr,"open file: %s failed",argv[1]);
     	return -1;
     }
 
-    while (1)
+    while (loops)
     {
-    	/* read data */
-    	rc = fread(buffer, 1, size, fp);
-    	if (rc == 0) // not data
-    	{
-    		fprintf(stderr, "end of file: %s\n",argv[1]);
-    		break;
-    	}
-    	else if (rc != size)
-    	{
-    		if (feof(fp))
-    			fprintf(stderr, "end of file: %s\n",argv[1]);
-    		else
-    			fprintf(stderr,"read file: %s error\n", argv[1]);
-
-    		break;
-    	}
-
-    	/* write to driver for playback */
-    	rc = snd_pcm_writei(handle, buffer, frames);
+        loops--;
+        rc = snd_pcm_readi(handle, buffer, frames);
     	if (rc == -EPIPE) // EPIPE means underrun
     	{
     		fprintf(stderr, "underrun occurred\n");
@@ -94,11 +83,19 @@ int main(int argc,const char *argv[])
     	}
     	else if (rc < 0)
     	{
-    		fprintf(stderr,"error from writei: %s\n",snd_strerror(rc));
+    		fprintf(stderr,"error from readi: %s\n",snd_strerror(rc));
     		break;
     	}
     	else if (rc != (int)frames)
         	fprintf(stderr,"short write, write %d frames\n", rc);
+    	
+        /* read data */
+    	rc = fwrite(buffer, 1, size, fp);
+    	if (rc != size)
+    	{
+    	    fprintf(stderr,"write file: %s error\n", argv[1]);
+    		break;
+    	}
     }
 
     fclose(fp);
@@ -108,3 +105,4 @@ int main(int argc,const char *argv[])
 
     return 0;
 }
+
